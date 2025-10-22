0cb0246c471004f573e9cd78da6e2df223b53332
Mihai Negrisan <mihainegrisan1@gmail.com>
2025-10-22 15:34:34 +0300

    Fix #15 Add LowStockWarningEvent

---
+++ Inventory/Events/LowStockWarningEvent.cs
@@ -0,0 +1,10 @@
+﻿using MFO.Contracts.Shared.Events;
+
+namespace MFO.Contracts.Inventory.Events;
+
+public record LowStockWarningEvent(
+    Guid ProductId,
+    string Sku,
+    int CurrentStockLevel,
+    int LowStockThreshold
+) : DomainEvent;
